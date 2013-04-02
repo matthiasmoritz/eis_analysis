b035d0139001e8f452a5b3b412070cf6c9e8fada
Matthias Moritz <moritz370@googlemail.com>
2013-04-02 18:31:15 +0200

    =first steps in gui things with qt4

--- ui.py
+++ ui.py
@@ -1,4 +1,31 @@
-
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
+
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self)
+
+        self.resize(350, 250)
+        self.setWindowTitle('mainwindow')
+
+        textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exit = QtGui.QAction(QtGui.QIcon(''), 'Exit', self)
+        exit.setShortcut('Ctrl+Q')
+        exit.setStatusTip('Exit application')
+        self.connect(exit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        file = menubar.addMenu('&File')
+        file.addAction(exit)
+
+
+app = QtGui.QApplication(sys.argv)
+main = MainWindow()
+main.show()
+sys.exit(app.exec_())
