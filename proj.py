84363b2f4b5a83d0d81ae7ff7d52b7d33b35dfb3
Matthias Moritz <moritz370@googlemail.com>
2013-04-04 18:39:32 +0200

    .

--- proj.py
+++ proj.py
@@ -21,8 +21,6 @@ class analyse:
         ff.setData(filename)
         self.Data.update ({os.path.splitext(filename)[0] : ff.Table})
         
-        for key in self.Data:
-            print (key)   
                 
     def makeP00s(self, subdir='P00'):
         if not (os.path.exists(self.Path + '/P00')):
